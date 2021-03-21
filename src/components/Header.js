45381086e16e703787f48794dab088866372314d
Ben Gittins <ben.gittins@dunelm.com>
2021-03-21 14:01:11 +0000

    Bare bones react setup and add first component

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+    return (
+      <>
+        <section className="header">
+            <div className='title'>
+                <h1>Risk Territory Assigner</h1>
+                <p>
+                    Randomly Allocate territories for your 2 - 6 players for your game of Risk. 
+                </p>
+            </div>
+        </section>
+      </>
+    );
+  };
+  
+  export default Header;
+  
