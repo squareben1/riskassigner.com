245f67046e20873e31cf4711e436dc7c604ff3aa
Ben Gittins <ben.gittins@dunelm.com>
2021-03-22 19:59:26 +0000

    Move state out of PlayerInput into App, pass numberChangeHandler down to PlayerInput component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import './styles/App.css';
-import Header from './components/Header'
-import PlayerInput from './components/PlayerInput'
+import React, { Component } from "react";
+import "./styles/App.css";
+import Header from "./components/Header";
+import PlayerInput from "./components/PlayerInput";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Header />
-        <PlayerInput />
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+      playerNumber: 2,
+    };
+
+  numberChangeHandler(event) {
+    this.setState({ playerNumber: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Header />
+          <PlayerInput numberChangeHandler={this.numberChangeHandler} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/PlayerInput.js
+++ src/components/PlayerInput.js
@@ -1,41 +1,30 @@
 import React, { Component } from "react";
 
-class PlayerInput extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      playerNumber: 2,
-    };
-  }
-  onNumberChange(event) {
-    this.setState({ playerNumber: event.target.value });
-  }
-
-  render() {
-    return (
-      <>
-        <section className="playerInput">
-          <div className="playerInputContainer">
-            <form className="player-form">
-              <label for="num_players">Players:</label>
-              <input
-                type="number"
-                name="num_players"
-                id="num_players"
-                placeholder="2"
-                min="2"
-                max="6"
-                onChange={this.onNumberChange.bind(this)}
-              />
-              <button type="submit" className="btn assign">
-                Assign
-              </button>
-            </form>
-          </div>
-        </section>
-      </>
-    );
-  }
-}
+const PlayerInput = (props) => {
+  return (
+    <>
+      <section className="playerInput">
+        <div className="playerInputContainer">
+          <form className="player-form">
+            <label for="num_players">Players:</label>
+            <input
+              type="number"
+              name="num_players"
+              id="num_players"
+              placeholder="2"
+              min="2"
+              max="6"
+              onChange={() => props.numberChangeHandler.bind(this)}
+            />
+            <button type="submit" className="btn assign">
+              Assign
+            </button>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+  // }
+};
 
 export default PlayerInput;
