26d872d5fd5b947be69471b72be48d7d5e7e20b5
Ben Gittins <ben.gittins@dunelm.com>
2021-04-07 12:58:03 -0700

    Test all permutations of shuffle() have same probability, refactor

--- src/helpers/territoryAssigner.js
+++ src/helpers/territoryAssigner.js
@@ -1,12 +1,12 @@
 const shuffle = (array) => {
-  let arr = array.slice()
+  let arr = array.slice();
 
   for (let i = arr.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
 
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
-  return arr
+  return arr;
 };
 
 module.exports = shuffle;
--- src/helpers/tests/territoryAssigner.test.js
+++ src/helpers/tests/territoryAssigner.test.js
@@ -1,9 +1,37 @@
+import shuffle from "../territoryAssigner";
 
-import shuffle from '../territoryAssigner';
+const array = [
+  "Kamchatka",
+  "Japan",
+  "Eastern Australia",
+  "Alaska",
+  "Northwest Territory",
+  "Iceland",
+];
+describe("#shuffle", () => {
+  it("Shuffle: returns shuffled array", () => {
+    expect(shuffle(array)).not.toEqual(array);
+  });
 
-const array = ['Kamchatka', 'Japan', 'Eastern Australia', 'Alaska', 'Northwest Territory', 'Iceland'];
+  const count = {
+    123: 0,
+    132: 0,
+    213: 0,
+    231: 0,
+    321: 0,
+    312: 0,
+  };
+  it("Shuffle: all permuattions have roughly same probability", () => {
+    for (let i = 0; i < 1000000; i++) {
+      let array = [1, 2, 3];
+      const arr = shuffle(array);
+      count[arr.join("")]++;
+    }
 
-it('shuffle function shuffles array', () => {
-  expect(shuffle(array)).not.toEqual(array)
+    for (let key in count) {
+      const diff = count[key] - 160000;
+      expect(diff).toBeGreaterThan(6000);
+      expect(diff).toBeLessThan(8000);
+    }
+  });
 });
-
